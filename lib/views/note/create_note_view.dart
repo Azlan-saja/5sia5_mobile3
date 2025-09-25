848f44b347c1d2c763d13df3cbd271dc052e77db
Azlan-saja <azlansaja19@gmail.com>
2025-09-25 09:17:24 +0700

    p12

---
+++ lib/views/note/create_note_view.dart
@@ -0,0 +1,65 @@
+import 'package:aplikasi_5sia5_mobile3/controllers/note_controller.dart';
+import 'package:flutter/material.dart';
+
+class CreateNoteView extends StatelessWidget {
+  const CreateNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final noteController = NoteController();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        foregroundColor: Colors.white,
+        title: Text('Create Note'),
+        actions: [
+          IconButton(
+              onPressed: () => noteController.prosesCreateData(context),
+              icon: Icon(Icons.check))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          key: noteController.formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: noteController.titleController,
+                validator: (value) =>
+                    noteController.cekValidasi(value, label: 'Title'),
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  hintText: "Title",
+                  prefixIcon: Icon(Icons.title),
+                  border: InputBorder.none,
+                  fillColor: Colors.grey.withAlpha(40),
+                  filled: true,
+                  contentPadding: EdgeInsets.only(top: 13),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                controller: noteController.contentController,
+                validator: (value) =>
+                    noteController.cekValidasi(value, label: 'Content'),
+                maxLines: 5,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  hintText: "Content",
+                  prefixIcon: Icon(Icons.notes),
+                  border: InputBorder.none,
+                  fillColor: Colors.grey.withAlpha(40),
+                  filled: true,
+                  contentPadding: EdgeInsets.only(top: 13),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/note/note_view.dart
+++ lib/views/note/note_view.dart
@@ -1,8 +1,25 @@
+import 'package:aplikasi_5sia5_mobile3/controllers/note_controller.dart';
+import 'package:aplikasi_5sia5_mobile3/models/node_model.dart';
 import 'package:aplikasi_5sia5_mobile3/views/login_view.dart';
+import 'package:aplikasi_5sia5_mobile3/views/note/create_note_view.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class NoteView extends StatelessWidget {
+class NoteView extends StatefulWidget {
   const NoteView({super.key});
+
+  @override
+  State<NoteView> createState() => _NoteViewState();
+}
+
+class _NoteViewState extends State<NoteView> {
+  final noteController = NoteController();
+
+  @override
+  void initState() {
+    super.initState();
+    noteController.init();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,21 +44,82 @@ class NoteView extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: TextFormField(
-          decoration: InputDecoration(
-            hintText: "Search Note",
-            prefixIcon: Icon(Icons.search),
-            border: InputBorder.none,
-            fillColor: Colors.grey.withAlpha(40),
-            filled: true,
-            contentPadding: EdgeInsets.only(top: 13),
-          ),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Search Note",
+                prefixIcon: Icon(Icons.search),
+                border: InputBorder.none,
+                fillColor: Colors.grey.withAlpha(40),
+                filled: true,
+                contentPadding: EdgeInsets.only(top: 13),
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder<List<NoteModel>>(
+                future: noteController.notes,
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<NoteModel>> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text("Data Kosong"));
+                  } else {
+                    final items = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: items.length,
+                      itemBuilder: (context, index) {
+                        final note = items[index];
+                        return AnimatedContainer(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeInOut,
+                          margin: const EdgeInsets.symmetric(vertical: 6),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            elevation: 4,
+                            shadowColor: Colors.black.withAlpha(124),
+                            child: ListTile(
+                              contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 6),
+                              title: Text(
+                                note.noteTitle,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                              subtitle: Text(
+                                DateFormat("yMMMd")
+                                    .format(DateTime.parse(note.createdAt)),
+                                style: const TextStyle(
+                                    color: Colors.grey, fontSize: 13),
+                              ),
+                              onTap: () {
+                                // Navigasi ke halaman lihat/update note
+                              },
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.teal,
         foregroundColor: Colors.white,
-        onPressed: () {},
+        onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CreateNoteView(),
+            )),
         child: Icon(Icons.add),
       ),
     );
